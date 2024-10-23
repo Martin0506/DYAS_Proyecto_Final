f01351d8d4709979d70ab61bfd077df7c9e48013
Lufe04 <luisacarpintero01@gmail.com>
2024-10-23 12:17:29 -0500

    primer commit

---
+++ trainlyapp/src/main/java/com/trainly/app/trainlyapp/TrainlyappApplication.java
@@ -0,0 +1,13 @@
+package com.trainly.app.trainlyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainlyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainlyappApplication.class, args);
+	}
+
+}
---
+++ trainlyapp/src/test/java/com/trainly/app/trainlyapp/TrainlyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trainly.app.trainlyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainlyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
