5b24788fb47ceb20479feeb073bd24fddea270b1
Martin0506 <jerezramirezmartin@gmail.com>
2024-11-03 17:18:52 -0500

    Base de Datos
    Creacion y configuracion base datos

---
+++ trainlyapp/src/main/java/com/trainly/app/trainlyapp/config/DatabaseConfig.java
@@ -0,0 +1,29 @@
+package com.trainly.app.trainlyapp.config;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+import java.io.InputStream;
+
+public class DatabaseConfig {
+    private static Connection connection = null;
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            try (InputStream input = DatabaseConfig.class.getClassLoader().getResourceAsStream("application.properties")) {
+                Properties prop = new Properties();
+                prop.load(input);
+
+                String url = prop.getProperty("db.url");
+                String username = prop.getProperty("db.username");
+                String password = prop.getProperty("db.password");
+
+                connection = DriverManager.getConnection(url, username, password);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return connection;
+    }
+}
